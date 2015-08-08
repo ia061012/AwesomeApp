5b2ebbaaabab29ab9e34b05db430109384e556fc
Wataru Kishimoto <w.kishimoto0215@gmail.com>
2015-08-08 20:13:35 +0900

    link deployGate

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 ./gradlew assembleRelease
-curl -F "file=@app/build/outputs/apk/app-release.apk" -F "token=${DEPLOY_GATE_API_KEY}" -      F "message=sample" https://deploygate.com/api/users/ia61012/apps
+curl -F "file=@app/build/outputs/apk/app-release.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=sample" https://deploygate.com/api/users/ia61012/apps
