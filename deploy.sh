54eeb94954a993b75c630c41267deef2771ed46c
Wataru Kishimoto <w.kishimoto0215@gmail.com>
2015-08-08 20:42:19 +0900

    link deployGate

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,6 @@
 ls -l app/build/outputs/apk/app-release.apk
 ls -l /home/ubuntu/AwesomeApp/app/build/outputs/apk/app-release.apk
 
-curl -F "file=@app/build/outputs/apk/app-release.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=sample" https://deploygate.com/api/users/ia61012/apps
+curl -F "file=@app/build/outputs/apk/app-release.apk" -F "token=${DEPLOY_GATE_API_KEY}" -F "message=sample" https://deploygate.com/api/users/ia061012/apps
 
 echo $?
